a37205fda7ebd0988f949ee92a1c3ab663107b75
dash7ou <fxfddfno@gmail.com>
2023-01-23 16:12:46 +0200

    get blog

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -18,14 +18,27 @@ def get_db():
 
 @app.get("/blog")
 def get_all(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
+    try:
+        blogs = db.query(models.Blog).all()
+        return blogs
+    except:
+        raise HTTPException(status_code=500, detail=f'Something go wrong!')
 
+@app.get("/blog/{id}")
+def get_one(id: int, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=404, detail=f'blog with id: {id} is not avalible')
 
-@app.post("/blog")
+    return blog
+
+@app.post("/blog", status_code=201)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title= request.title, body=request.body)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+    try:
+        new_blog = models.Blog(title= request.title, body=request.body)
+        db.add(new_blog)
+        db.commit()
+        db.refresh(new_blog)
+        return new_blog
+    except:
+        raise HTTPException(status_code=500, detail=f'Something go wrong!')
